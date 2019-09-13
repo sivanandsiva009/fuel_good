fd6385c849745c5fec479130427dda0dfa1b8114
prasanth99 <prasanth.mallan@gmail.com>
2019-09-14 04:31:43 +0530

    signup bare bones

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(express.static(__dirname+"/public"));
 app.get("/",function(req,res){
     res.render("index");
 });
+app.get("/signup",function(req,res){
+    res.render("signup");
+});
 
 app.listen(1690,function(){
     console.log("Runnning on 1690");
